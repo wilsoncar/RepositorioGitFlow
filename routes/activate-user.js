0add7cd7e57a5ec13ceeebf5cb5b5359860a04cf
Rigo <rigo.vogst@gmail.com>
2022-01-28 21:55:15 -0600

    Revert "This is an example of how to use a git command"
    This reverts commit 3438e7ca138e59b0b601e5e59409dd0cc66cc14f.

--- routes/activate-user.js
+++ routes/activate-user.js
@@ -7,7 +7,6 @@ function userApi(app) {
     app.use("/api/activate-user", router);
 
     const userService = new UserService();
-    console.log('userService', userService);
 
     router.put("/", async function (req, res, next) {
         const { body: user } = req;
