d84c1d7caa958cef29b686f5d395a22db20f707f
Rigo <rigo.vogst@gmail.com>
2022-01-31 18:34:45 -0600

    Test

--- routes/activate-user.js
+++ routes/activate-user.js
@@ -7,6 +7,7 @@ function userApi(app) {
     app.use("/api/activate-user", router);
 
     const userService = new UserService();
+    console.log();
 
     router.put("/", async function (req, res, next) {
         const { body: user } = req;
